3af78737d1b853619c191b652ca0eceb2b07deaf
Jonathan Lipps <jlipps@gmail.com>
2023-08-24 11:57:43 -0700

    add new favicon and analytics script

--- app/head.mjs
+++ app/head.mjs
@@ -3,15 +3,31 @@ import { getStyles }  from '@enhance/arc-plugin-styles'
 
 const { linkTag } = getStyles
 
+const isProd = process.env.ARC_ENV === 'production'
+
 /** @type {import('@enhance/types').EnhanceHeadFn} */
 export default function Head(state) {
   const {store} = state
   const {title} = store
   const titleExtra = title ? `: ${title}` : ''
+
+  const gtagHtml = isProd ? `
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T0CKBF7PMX"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-T0CKBF7PMX');
+    </script>
+  ` : ''
+
   return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
+      ${gtagHtml}
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Jonathan Lipps${titleExtra}</title>
